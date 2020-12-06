00216a5111997fb8c14d01c258bdc68676c60f92
Allan Rezende <allan.rezende@outlook.com>
2020-12-06 10:09:09 -0300

    JavaScript

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//var nome = "Allan Rezende Moreira";
+//var idade = 29;
+//var idade2 = 10;
+//var frase = "PS5 é o melhor console do mundo"
+//alert(nome + " tem " + idade + " anos");
+//alert (idade + idade2);
+//console.log(nome);
+//console.log(idade+idade2);
+//console.log(frase.replace("PS5", "Xbox Series X"));
+//alert(frase.replace("PS5", "Xbox Series X"));
+//console.log(frase.toUpperCase());
+
+//var lista = ["maça", "pera", "laranja"];
+//console.log(lista[1]);
+//lista.push("uva");
+//console.log(lista);
+// lista.pop();
+// console.log(lista);
+// console.log(lista.length);
+// console.log(lista.reverse());
+// console.log(lista[0]);
+// console.log(lista.toString()[0]); //Mostra primeira letra
+// console.log(lista.join(" - ")) // altera o separador dos elementos de um array
+
+// var fruta = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+// console.log(fruta);
+// alert(fruta.cor);
+
+var idade = 18
+if(idade > 18){
+    alert("Maior de idade")
+}else{
+    alert("menor de Idade")
+}
+
